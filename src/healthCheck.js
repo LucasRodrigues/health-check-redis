0b73d9aac17aebdc38df2cf2495a48057e5068fd
Lucas Rodrigues <lucas.carvalho.rodrigues@gmail.com>
2015-12-16 23:29:21 -0200

    Add suport to array or object

--- src/healthCheck.js
+++ src/healthCheck.js
@@ -3,7 +3,16 @@ import Format from './format';
 
 export default class HealthCheck {
 
+  static _resolveParameter (configurations){
+    if(!Array.isArray(configurations)){
+      configurations = [configurations];
+    }
+
+    return configurations
+  }
+
   static do(configurations) {
+    configurations = this._resolveParameter(configurations);
     return new Promise((resolve, reject) => {
       let promises = [];
 
