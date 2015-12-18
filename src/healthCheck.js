dff7ff62fd67d0015e1725b675fa4eb31392ac6b
Lucas Rodrigues <lucas.carvalho.rodrigues@gmail.com>
2015-12-17 23:04:40 -0200

    Add core on project to minimize repeat code

--- src/healthCheck.js
+++ src/healthCheck.js
@@ -1,41 +1,26 @@
 import Repository from './repository';
-import HealthCheckMessageFormat from 'health-check-message-format';
+import HealthCheckCore from 'health-check-core';
 
 export default class HealthCheck {
 
-  static _resolveParameter(configurations) {
-    if (!Array.isArray(configurations)) {
-      configurations = [configurations];
-    }
-
-    return configurations;
-  }
-
   static do(configurations) {
-    configurations = this._resolveParameter(configurations);
     return new Promise((resolve, reject) => {
-      let promises = [];
+      const configuration = {
+        fnIsHealthGood: status => {
+          return (status.error === null);
+        },
+        fnName: status => {
+          return `${status.configuration.host}:` +
+            `${status.configuration.port}`;
+        },
+        fnErrorMessage: status => {
+          return status.error.message;
+        }
+      };
 
-      configurations.forEach(configuration => {
-        promises.push(Repository.test(configuration));
-      });
-
-      Promise.all(promises)
-        .then(statuses => {
-          const configuration = {
-            fnIsHealthGood: status => {
-              return (status.error === null);
-            },
-            fnName: status => {
-              return `${status.configuration.host}:
-              ${status.configuration.port}`;
-            },
-            fnErrorMessage: status => {
-              return status.error.message;
-            }
-          };
-
-          resolve(HealthCheckMessageFormat.do(statuses, configuration));
+      HealthCheckCore.do(configurations, Repository.test, configuration)
+        .then(result => {
+          resolve(result);
         })
         .catch(error => {
           reject(error);
